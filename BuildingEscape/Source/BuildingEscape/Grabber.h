f464c752e7b65ceaf73d2f4dc0c48010b28a6178
PsychoTaker <teufik.cajlakovic@gmail.com>
2017-02-13 22:12:22 +0100

    End of Mid Section, Quiz 8 <including materials>

---
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+private:
+	float Reach = 100.f;
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	UInputComponent* InputComponent = nullptr;
+	
+	// Ray-cast and grab what's in reach
+	void Grab();
+	// Called when grab is released
+	void Release();
+};
